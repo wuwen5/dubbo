25666fce4251be634bbd3fbf565b9ab2e8b2c02e
Albumen Kevin <jhq0812@gmail.com>
2023-09-22 20:23:32 +0800

    Enhance unit test cases from scheduled tasks (#13099)

--- dubbo-remoting/dubbo-remoting-netty4/src/test/java/org/apache/dubbo/remoting/transport/netty4/NettyClientToServerTest.java
+++ dubbo-remoting/dubbo-remoting-netty4/src/test/java/org/apache/dubbo/remoting/transport/netty4/NettyClientToServerTest.java
@@ -38,7 +38,7 @@ class NettyClientToServerTest extends ClientToServerTest {
 
     protected ExchangeChannel newClient(int port) throws RemotingException {
         // add heartbeat cycle to avoid unstable ut.
-        URL url = URL.valueOf("exchange://localhost:" + port + "?client=netty4&timeout=3000");
+        URL url = URL.valueOf("exchange://localhost:" + port + "?client=netty4&timeout=300000");
         url = url.addParameter(Constants.HEARTBEAT_KEY, 600 * 1000);
         return Exchangers.connect(url);
     }
